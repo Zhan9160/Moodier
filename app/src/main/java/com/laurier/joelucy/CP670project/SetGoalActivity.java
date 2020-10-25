c430f42933b1413f0c7dab58cb5b366d93b85041
PeiyuLu <luxx5940@mylaurier.ca>
2020-10-25 03:24:52 -0400

    add some button and testview for three fragments

---
+++ app/src/main/java/com/laurier/joelucy/CP670project/SetGoalActivity.java
@@ -0,0 +1,14 @@
+package com.laurier.joelucy.CP670project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SetGoalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_goal);
+    }
+}
