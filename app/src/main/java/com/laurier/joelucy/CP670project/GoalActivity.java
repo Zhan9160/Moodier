89524926cb74ec66e6e1011a0dfb2242815b843d
PeiyuLu <luxx5940@mylaurier.ca>
2020-10-28 08:27:47 -0400

    add buttons and slider for each fragment; add transfer from main activity to "set goal" and "log mood" activity; add application icon image.

--- app/src/main/java/com/laurier/joelucy/CP670project/GoalActivity.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/GoalActivity.java
@@ -2,10 +2,13 @@ package com.laurier.joelucy.CP670project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class GoalActivity extends AppCompatActivity {
 
--- app/src/main/java/com/laurier/joelucy/CP670project/LogMoodActivity.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/LogMoodActivity.java
@@ -2,13 +2,21 @@ package com.laurier.joelucy.CP670project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class LogMoodActivity extends AppCompatActivity {
-
+    private static final String ACTIVITY_NAME ="LogMoodActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_mood);
+    }
+    public void get_logmood_activity(View view){
+        Intent log_mood_intent = new Intent(LogMoodActivity.this,LogMoodActivity.class);
+        startActivity(log_mood_intent);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
     }
 }
