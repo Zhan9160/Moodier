6d865621582309b9ff42aed579826292ece31815
PeiyuLu <luxx5940@mylaurier.ca>
2020-12-05 10:19:47 -0500

    re-arrange strings.xml

--- app/src/main/java/com/laurier/joelucy/CP670project/MainActivity.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/MainActivity.java
@@ -1,12 +1,18 @@
 package com.laurier.joelucy.CP670project;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
@@ -28,10 +34,26 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_home, R.id.navigation_goals, R.id.navigation_mood)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-       // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        // NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
-    }
 
+//        findViewById(R.id.set_mood_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent log_mood_intent = new Intent(MainActivity.this,LogMoodActivity.class);
+//                startActivity(log_mood_intent);
+//                Log.i(ACTIVITY_NAME, "In onCreate()");
+//            }
+//        });
+//        findViewById(R.id.set_goal_button).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent set_goal_intent = new Intent(MainActivity.this,SetGoalActivity.class);
+//                startActivity(set_goal_intent);
+//                Log.i(ACTIVITY_NAME, "In onCreate()");
+//            }
+//        });
+    }
     //go to set goals page
     public void get_setgoal_activity(View view){
         Intent set_goal_intent = new Intent(MainActivity.this,SetGoalActivity.class);
@@ -44,5 +66,11 @@ public class MainActivity extends AppCompatActivity {
         startActivity(log_mood_intent);
         Log.i(ACTIVITY_NAME, "In onCreate()");
     }
+    public void go_to_moodhistory(View view){
+        Intent log_mood_intent = new Intent(MainActivity.this,ListMoodRecord.class);
+        startActivity(log_mood_intent);
+        Log.i(ACTIVITY_NAME, "In onCreate()");
+    }
+
 
 }
--- app/src/main/java/com/laurier/joelucy/CP670project/ui/home/HomeFragment.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/ui/home/HomeFragment.java
@@ -4,32 +4,51 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.viewpager.widget.ViewPager;
 
 import com.laurier.joelucy.CP670project.R;
+
+import java.util.ArrayList;
+import java.util.concurrent.ScheduledExecutorService;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
+    ArrayList<ImageView> imageViews= new ArrayList<>();
+    ArrayList<View>dots=new ArrayList<>();
+    ViewPager vp;
+    int[] image = {R.drawable.main1, R.drawable.main2, R.drawable.main3, R.drawable.main4};
+    int oldPosition=0;
+    int currentItem;
+    ScheduledExecutorService scheduledExecutorService;
+
+//    protected void onCreate(Bundle saveInstanceState){
+//
+//    }
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         homeViewModel =
-                ViewModelProviders.of(this).get(HomeViewModel.class);
+               new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.fragment_home, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
-        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+//        final TextView textView = root.findViewById(R.id.text_home);
+//        homeViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+//            @Override
+//            public void onChanged(@Nullable String s) {
+//                textView.setText(s);
+//            }
+//        });
+
         return root;
     }
+
 }
