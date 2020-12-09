f40913a5c4d0603b96f23af2154fde6b99b7db36
PeiyuLu <luxx5940@mylaurier.ca>
2020-12-09 06:39:00 -0500

    has problem with mood test

--- app/src/main/java/com/laurier/joelucy/CP670project/MoodResult.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/MoodResult.java
@@ -2,16 +2,22 @@ package com.laurier.joelucy.CP670project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.laurier.joelucy.CP670project.ui.mood.MoodFragment;
 
 public class MoodResult extends AppCompatActivity {
     TextView textInput;
+    String result;
 //    Button submit;
 
 
@@ -21,18 +27,36 @@ public class MoodResult extends AppCompatActivity {
         setContentView(R.layout.activity_mood_result);
 
         Intent intent =getIntent();
-        String result = intent.getStringExtra("result");
+        result = intent.getStringExtra("result");
         textInput = findViewById(R.id.mood_result);
         textInput.setText(result);
+
+        InitEvents();
 
         findViewById(R.id.mood_result_back_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent back_intent = new Intent(MoodResult.this,MainActivity.class);
+//                SharedPreferences mySharedPreferences = getSharedPreferences("moodResult", MODE_PRIVATE);
+//                SharedPreferences.Editor editor = mySharedPreferences.edit();
+//                editor.putString("mood", result);
+//                editor.commit();
+                Intent back_intent = new Intent(MoodResult.this,MoodFragment.class);
+//                Intent back_intent = new Intent(MoodResult.this,MainActivity.class);
+                back_intent.putExtra("result",result);
                 startActivity(back_intent);
             }
         });
 
     }
+
+    private void InitEvents() {
+        SharedPreferences sharedPreferences = this.getSharedPreferences("moodResult", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("result", result);
+        editor.commit();
+//        Intent intent = new Intent(MoodResult.this, CreateNewGoal.class);
+//        startActivity(intent);
+    }
+
 
 }
--- app/src/main/java/com/laurier/joelucy/CP670project/ui/mood/MoodFragment.java
+++ app/src/main/java/com/laurier/joelucy/CP670project/ui/mood/MoodFragment.java
@@ -6,6 +6,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -14,14 +15,22 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
+import com.laurier.joelucy.CP670project.ListMoodRecord;
 import com.laurier.joelucy.CP670project.LogMoodActivity;
 import com.laurier.joelucy.CP670project.MainActivity;
 import com.laurier.joelucy.CP670project.R;
 import com.laurier.joelucy.CP670project.WriteMood;
 
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
+
 public class MoodFragment extends Fragment {
 
     private MoodViewModel notificationsViewModel;
+    String result;
+    ListView listView;
+    private ArrayList<String> list;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -37,6 +46,18 @@ public class MoodFragment extends Fragment {
         });
 //        View.OnClickListener listener = new
 //        root.findViewById(R.id.set_mood_button)
+        Intent intent =getActivity().getIntent();
+        result = intent.getStringExtra("result");
+        TextView tv = root.findViewById(R.id.recyclerView_moodResult);
+        tv.setText(result);
+
+//        listView = root.findViewById(R.id.recyclerView_moodRecord);
+//        list = new ArrayList<>();
+//
+//        messageAdapter =new ListMoodRecord.MoodAdapter( this );
+//        listView.setAdapter(messageAdapter);
+//        View view = root.findViewById(R.id.recyclerView_moodResult);
+//        listView.setAdapter(messageAdapter);
 
         return root;
     }
